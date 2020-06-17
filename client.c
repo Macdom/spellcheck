aaea0d221f011b1de82b831a4d7f0f92d37bb70b
Maciej Domagalski <ikslamabad@gmail.com>
2020-06-17 10:28:13 +0200

    rewritten to UNIX socket

--- client.c
+++ client.c
@@ -1,55 +1,55 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int prepareAndOpenSocket(char* name){
+#define ADDRESS "/tmp/spellSock"
+
+int prepareAndOpenSocket(){
 	
-	struct sockaddr_un servinfo;
-	int sock = socket(PF_UNIX, SOCK_STREAM, 0);
-	if(sock != 0){
-		printf("%s\n", strerror(sock));
+	int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(sock < 0){
 		perror ("Couldn't open socket");
 		exit(1);
 	}
 	
-	memset(&servinfo, 0, sizeof(struct sockaddr_un));
+	struct sockaddr_un servinfo;
+	//memset(&servinfo, 0, sizeof(struct sockaddr_un));
 	servinfo.sun_family = AF_UNIX;
-	strncpy(servinfo.sun_path, name, sizeof(servinfo.sun_path) - 1);
+	
+	strcpy(servinfo.sun_path, ADDRESS);
 	printf("Socket open\n");
-
+	
 	// connect to server
-	if(connect(sock, (struct sockaddr*)&servinfo, SUN_LEN(&servinfo) < 0)){
+	if(connect(sock, (struct sockaddr*)&servinfo, SUN_LEN(&servinfo)) == -1){
 		close(sock);
 		perror("Failed to connect");
 		exit(3);
 	}
 	
+	printf("Connected\n");	
 	return sock;
-	printf("Connected\n");
 }
 
 int main(int argc, char ** argv){
 	
-	char PORT[4], word[30];
+	char word[30];
 	// check argument count
-	if (argc != 3){
-		printf("Usage: ./client port word\n");
+	if (argc != 2){
+		printf("Usage: ./client word\n");
 		exit(-1);
 	}
 	else{
-		// resolve port number
-		strcpy(PORT, argv[1]);
 		// read the word to spellcheck
-		strcpy(word, argv[2]);
+		strcpy(word, argv[1]);
 	}
 
-	int sock = prepareAndOpenSocket("spellSock");
+	int sock = prepareAndOpenSocket();
 	
 	// send the word
 	write(sock, word, strlen(word) + 1);
